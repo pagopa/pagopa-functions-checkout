4f9fded59b8219fa44f1eea59a6fd74e159a7c31
Marco Greco <marcoandreagreco@gmail.com>
2021-11-16 15:03:28 +0100

    fix: dockerized pagopa-proxy, add test RPTID handler

--- pagopaproxy-mock-server.js
+++ pagopaproxy-mock-server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require('morgan')
 
 const app = express();
 
 app.use(express.json()) 
+app.use(morgan('combined'))
 
 app.get("/api/v1/payment-requests/:rptId", (_, res) => {
   // test scenario for an error message
